52b312307412fc6a3135d752006cb52624d03b30
r2DoesInc <r2doesinc@gmail.com>
2011-02-03 18:35:35 -0800

    fix copy path

--- build_kernel.sh
+++ build_kernel.sh
@@ -11,4 +11,4 @@ make ARCH=arm jt1134_galaxytab_defconfig
 make -j8 CROSS_COMPILE="$TOOLCHAIN" \
 	ARCH=arm
 
-mv arch/arm/boot/zImage ../kernel/tab-kernels/t3hh4xx0r_"$DEVICE"_Honeycomb_"$DATE"_zImage
+mv arch/arm/boot/zImage ../tab-kernels/t3hh4xx0r_"$DEVICE"_Honeycomb_"$DATE"_zImage
